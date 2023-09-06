8b6beeb6228c6dd9a0e5957ac8993491306c4ebe
upioneer <hgrand@outlook.com>
2023-09-06 08:01:20 -0500

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -11,20 +11,20 @@
 
     initialize
     oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json"
-    (@(& 'C:/Users/hgran/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\hgran\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' --print) -join "`n") | Invoke-Expression
+    (@(& 'C:/Users/USER/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe' init pwsh --config='C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' --print) -join "`n") | Invoke-Expression
 
     themes
     Get-PoshThemes
 
     set new theme (point to new desired theme json)
-    oh-my-posh init pwsh --config 'C:\Users\hgran\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' | Invoke-Expression
+    oh-my-posh init pwsh --config 'C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' | Invoke-Expression
 
     edit profile .ps1 to automatically load the theme on startup
-    C:\Users\hgran\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+    C:\Users\USER\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
     notepad $PROFILE
 
     ex.
-    oh-my-posh init pwsh --config 'C:\Users\hgran\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' | Invoke-Expression
+    oh-my-posh init pwsh --config 'C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\jandedobbeleer.omp.json' | Invoke-Expression
 
     source for anti-virus whitelist/exclusions
     (Get-Command oh-my-posh).Source
